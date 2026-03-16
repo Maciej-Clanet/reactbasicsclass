85adf5ae082fb1adeb060a58c52d91f9eef38965
Maciej-Clanet <maciejclanet@gmail.com>
2026-03-16 15:02:16 +0000

    asfasf

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,25 @@ import Search from "../Search/Search"
 import MenuIcon from "../../assets/menuicon.svg"
 import { useState } from "react"
 
+function NavDropdown({ text, children }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="nav-dropdown" onMouseLeave={() => setIsOpen(false)}>
+            <button className="nav-link" onMouseEnter={() => setIsOpen(true)}>{text}</button>
+            {
+                isOpen ? <div className="dropdown-content">
+                    {children}
+                </div> : null
+            }
+        </div>
+    )
+}
+
+
 function DesktopHeader() {
     return (
         <header className="main-header">
-
             <div className="nav-col">
                 <div className="logo">LogoText</div>
             </div>
@@ -14,6 +29,10 @@ function DesktopHeader() {
                 <Search />
             </div>
             <nav className="nav-col nav-btns">
+                <NavDropdown text="categories">
+                    <a className="nav-link" href="#">FASION</a>
+                    <a className="nav-link" href="#">GARDENING</a>
+                </NavDropdown>
                 <a className="nav-link" href="#">HOME</a>
                 <a className="nav-link" href="#">ABOUT</a>
                 <a className="nav-link" href="#">PRODUCTS</a>
